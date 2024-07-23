2075012127029b752e439a47cee4cf7f8ea2a46c
mshei <msheikh544@gmail.com>
2024-07-24 02:06:26 +0530

    2nd one

--- src/ex_240724/Lab_003.java
+++ src/ex_240724/Lab_003.java
@@ -1,5 +1,7 @@
+package ex_240724;
+
 public class Lab_003 {
     public static void main(String[] args) {
-        System.out.println("Mohsin12234");
+        System.out.println("Miya bhai";
     }
 }
